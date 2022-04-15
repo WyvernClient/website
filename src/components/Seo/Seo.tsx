c957b2b779321bd4f53288d16a3abf1e9fcb8ffc
jerbear2008 <jeremyrwoolley@gmail.com>
2022-04-15 07:40:41 -0700

    run yarn lint

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -2,28 +2,30 @@ import type React from 'react'
 import Head from 'next/head'
 
 const Seo: React.FC<{
-  siteName?: string,
-  title?: string,
-  description?: string,
-  smallImg?: string,
-  color?: string,
+	siteName?: string
+	title?: string
+	description?: string
+	smallImg?: string
+	color?: string
 }> = ({
-  siteName = 'Wyvern',
-  title = 'Wyvern Client',
-  description = '',
-  smallImg = 'https://wyvern.tk/icon.png',
-  color = '#3cb9ba',
+	siteName = 'Wyvern',
+	title = 'Wyvern Client',
+	description = '',
+	smallImg = 'https://wyvern.tk/icon.png',
+	color = '#3cb9ba',
 }) => {
-  return (
-    <Head>
-      <title>{title} | {siteName}</title>
-      <meta content={siteName} property="og:site_name"/>
-      <meta content={title} property="og:title"/>
-      <meta content={description} property="og:description"/>
-      <meta content={smallImg} property="og:image"/>
-      <meta content={color} name="theme-color"/>
-    </Head>
-  )
+	return (
+		<Head>
+			<title>
+				{title} | {siteName}
+			</title>
+			<meta content={siteName} property="og:site_name" />
+			<meta content={title} property="og:title" />
+			<meta content={description} property="og:description" />
+			<meta content={smallImg} property="og:image" />
+			<meta content={color} name="theme-color" />
+		</Head>
+	)
 }
 
 export default Seo
