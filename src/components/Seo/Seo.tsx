383b6c0563020614d4376f35f4a69ddaeb8f99b3
jerbear2008 <jeremyrwoolley@gmail.com>
2022-04-15 07:37:54 -0700

    add color to embed

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -2,22 +2,28 @@ import type React from 'react'
 import Head from 'next/head'
 
 const Seo: React.FC<{
-	siteName?: string
-	title?: string
-	description?: string
-	smallImg?: string
-}> = ({ siteName = 'Wyvern', title = 'Wyvern Client', description = '', smallImg = 'https://wyvern.tk/icon.png' }) => {
-	return (
-		<Head>
-			<title>
-				{title} | {siteName}
-			</title>
-			<meta content={siteName} property="og:site_name" />
-			<meta content={title} property="og:title" />
-			<meta content={description} property="og:description" />
-			<meta content={smallImg} property="og:image" />
-		</Head>
-	)
+  siteName?: string,
+  title?: string,
+  description?: string,
+  smallImg?: string,
+  color?: string,
+}> = ({
+  siteName = 'Wyvern',
+  title = 'Wyvern Client',
+  description = '',
+  smallImg = 'https://wyvern.tk/icon.png',
+  color = '#3cb9ba',
+}) => {
+  return (
+    <Head>
+      <title>{title} | {siteName}</title>
+      <meta content={siteName} property="og:site_name"/>
+      <meta content={title} property="og:title"/>
+      <meta content={description} property="og:description"/>
+      <meta content={smallImg} property="og:image"/>
+      <meta content={color} name="theme-color"/>
+    </Head>
+  )
 }
 
 export default Seo
