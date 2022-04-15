5f6466125ad1c903b50d72a81d1fa73548e3600f
jerbear4328 <iamjerbear4328@gmail.com>
2022-04-15 10:43:29 -0700

    Add more SEO meta tags

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -11,22 +11,45 @@ const Seo: React.FC<{
 }> = ({
 	siteName = 'Wyvern',
 	title = 'Wyvern Client',
+	homePage = 'https://wyvern.tk',
 	description = '',
-	image = 'https://wyvern.tk/icon.png',
+	longDescription = description,
+	creator = siteName,
+	icon = 'https://wyvern.tk/icon.png',
+	image = icon,
 	bigImage = false,
 	color = '#3cb9ba',
+	contact = '',
+	license = '',
+	licenseUrl = '',
+	twitterUsername = '',
 }) => {
 	return (
 		<Head>
 			<title>
 				{title} | {siteName}
 			</title>
+			<meta content={siteName} name="application-name" />
+			<meta contnet={homePage} name="application-url" />
+			<meta content={contact} name="contact" />
 			<meta content={title} property="og:title" />
-			<meta content={description} property="og:description" />
+			<meta content={description} name="description" />
+			<meta content={longDescription} property="og:description" />
+			<meta content={creator} name="creator" />
 			<meta content={image} property="og:image" />
 			{bigImage ? <meta name="twitter:card" content="summary_large_image" /> : ''}
 			<meta content={color} name="theme-color" />
-			<link type="application/json+oembed" href="/oembed.json" />
+			<meta content={license} name="license" />
+			<meta content={licenseUrl} name="license:uri" />
+			<meta content="summary" name="twitter:card" />
+			<meta content={twitterUsername} name="twitter:creator" />
+			<meta content={twitterUsername} name="twitter:site" />
+			<meta content={longDescription} name="twitter:description" />
+			<meta content={image} name="twitter:image" />
+			<link href={contact} rel="author" />
+			<link href={icon} rel="icon" />
+			<link href={licenseUrl} rel="licese" />
+			<link href="/oembed.json" type="application/json+oembed" />
 		</Head>
 	)
 }
