5015f8052151aee54bb6af4841a0a8ceacf91695
jerbear2008 <jeremyrwoolley@gmail.com>
2022-04-14 20:30:28 -0700

    add Seo

---
+++ src/components/Seo/Seo.tsx
@@ -0,0 +1,26 @@
+import type React from 'react'
+import Head from 'next/head'
+
+const Seo: React.FC<{
+  siteName?: string,
+  title?: string,
+  description?: string,
+  smallImg?: string,
+}> = ({
+  siteName = 'Wyvern',
+  title = 'Wyvern Client',
+  description = '',
+  smallImg = 'https://wyvern.tk/icon.png',
+}) => {
+  return (
+    <Head>
+      <title>{title} | {siteName}</title>
+      <meta content={siteName} property="og:site_name"/>
+      <meta content={title} property="og:title"/>
+      <meta content={description} property="og:description"/>
+      <meta content={smallImg} property="og:image"/>
+    </Head>
+  )
+}
+
+export default Seo
